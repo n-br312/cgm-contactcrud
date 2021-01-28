028834d1e27e35dd6a9bf59ff6b225c1fd4792ff
Backenkoehler <n.backenkoehler@tu-braunschweig.de>
2021-01-28 23:52:24 +0100

    Move delete button from patient list to detail view, remove edit button from list.

--- src/main/webapp/app/entities/patient/patient-detail.component.ts
+++ src/main/webapp/app/entities/patient/patient-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IPatient } from 'app/shared/model/patient.model';
+import { PatientDeleteDialogComponent } from 'app/entities/patient/patient-delete-dialog.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'jhi-patient-detail',
@@ -10,7 +12,7 @@ import { IPatient } from 'app/shared/model/patient.model';
 export class PatientDetailComponent implements OnInit {
   patient: IPatient | null = null;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, protected modalService: NgbModal) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ patient }) => (this.patient = patient));
@@ -18,5 +20,10 @@ export class PatientDetailComponent implements OnInit {
 
   previousState(): void {
     window.history.back();
+  }
+
+  delete(patient: IPatient): void {
+    const modalRef = this.modalService.open(PatientDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.patient = patient;
   }
 }
